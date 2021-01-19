a4a4267a7c821941678d6f2b0b1132776e9b7d40
Inzamam Baig <baigsone@gmail.com>
2021-01-20 02:10:09 +0500

    Added Hero Section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,33 @@
 import React from "react";
-
+import homepage from "../assets/videos/homepage.mp4";
 const Hero = () => {
   return (
-    <div>
-      <h1>I am a hero</h1>
+    <div className="container-fluid">
+      <div className="row">
+        <header>
+          <video
+            playsinline="playsinline"
+            autoplay="autoplay"
+            muted="muted"
+            loop="loop"
+          >
+            <source src={homepage} type="video/mp4" />
+          </video>
+          <div className="container h-100">
+            <div className="d-flex h-100 text-center align-items-center">
+              <div className="w-100 text-white">
+                <h1 className="display-3">
+                  Impartial, Remote, 3rd-Party, <br></br>
+                  Estimating Engine
+                </h1>
+                <div className="middle">
+                  <div className="mouse"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
     </div>
   );
 };
