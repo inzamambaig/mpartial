569874a4ce4d51953d65af885333b3f5727322b3
Inzamam Baig <baigsone@gmail.com>
2021-01-23 20:04:53 +0500

    Hero section updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,6 +8,7 @@ const Hero = () => {
           <video
             playsInline="playsinline"
             autoPlay="autoplay"
+            preload="auto"
             muted="muted"
             loop="loop"
           >
